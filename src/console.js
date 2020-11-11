009d37df977a8e5170b4481b8a81feca3ed2b95f
Jan T. Sott <jan@motim.co>
2020-11-11 09:52:29 +0100

    Destroy consoleView is exists
    I'm following this PR from your `tool-bar` package here
    
    https://github.com/suda/tool-bar/pull/326/commits/c71df992168c0d0f634882b4323b30210cfe1705

--- src/console.js
+++ src/console.js
@@ -26,7 +26,8 @@ export default {
 
 	deactivate() {
 		this.subscriptions.dispose();
-		this.consoleView.destroy();
+		this.consoleView?.destroy();
+		this.consoleView = null;
 	},
 
 	provideConsolePanel() {
